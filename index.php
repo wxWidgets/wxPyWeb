63ca1fbaf40e377defee5ab73c5a2ae164cf501d
Robin Dunn <robin@alldunn.com>
2008-04-16 20:16:15 +0000

    Free ebook promotion
    git-svn-id: https://svn.wxwidgets.org/svn/wxPyWeb/trunk@26 dae2d619-a531-0410-97b3-f94ca8335064

--- index.php
+++ index.php
@@ -99,9 +99,16 @@ Affiliate Links:
 <br>
 <br>
 
+<!--
 Work on wxPython is sponsored by:
 <p>
 <div align="center"><a href="http://www.osafoundation.org/"><img src="images/OSAFLogo.gif" width="559" height="64" alt="" border="0"></a></div>
+-->
+
+
+<a href="http://www.manning.com/affiliate/idevaffiliate.php?id=189_118_1_27"><img src="http://www.manning.com/affiliate/banners/Free ebook ad 5.gif" border="0"></a>
+
+
 
 <?php require('_footer.in') ?>
 

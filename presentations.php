3d913f56d5a6c0b3f416bbfa98455cc934cdd018
Robin Dunn <robin@alldunn.com>
2008-07-23 15:11:36 +0000

    OSCON 2008
    git-svn-id: https://svn.wxwidgets.org/svn/wxPyWeb/trunk@37 dae2d619-a531-0410-97b3-f94ca8335064

--- presentations.php
+++ presentations.php
@@ -10,6 +10,7 @@
     <a href="OSCON2003">OSCON 2003</a><br>
     <a href="OSCON2004">OSCON 2004</a><br>
     <a href="OSCON2006">OSCON 2006</a><br>
+    <a href="OSCON2008">OSCON 2008</a><br>
 </ul><p>
 
 <?php require('_footer.in') ?>

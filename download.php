be44eda8f08825ce365462d8b8f86384983d1d26
Robin Dunn <robin@alldunn.com>
2013-12-29 00:44:36 +0000

    3.0 Release
    git-svn-id: https://svn.wxwidgets.org/svn/wxPyWeb/trunk@83 dae2d619-a531-0410-97b3-f94ca8335064

--- download.php
+++ download.php
@@ -9,241 +9,15 @@
 <img src="images/download.jpg" width="505" height="73" alt="" border="0"><br>
 <p>
  
-<a name="stable"></a>
-<table width="100%" border="1" cellspacing="0" cellpadding="8" bgcolor="#BCB8E7">
-<tr><td>
-<font face="sans-serif" size="+1"><b>Old Stable Release</b></font><br>
-The files in this section are for the most recent release in the "stable" <a href="http://wiki.wxpython.org/ReleaseSeries">release series</a> of wxPython, version <b>2.8.12.1</b>.  In this context, "stable" means that the APIs are mostly frozen and backwards compatible between subreleases, not neccessarily any more or less buggy than the development series. The files for the current development release, <b>2.9.5.0</b>, are available <a href="#devel">below</a>.
-</td></tr>
-<tr><td>
-
-
-<p>
-<font face="sans-serif" size="+1"><b>Windows Binaries</b></font><br>
-
-Choose an installer that matches the version of Python you will be using.  If you are using a 64-bit version of Python then make sure you also get a 64-bit wxPython, otherwise choose a 32-bit installer even if you are on a 64-bit version of Windows.  Get the unicode build unless you know for sure that you need the ansi version, the ansi builds are in the process of being phased out.
-
-
-<p><div style="padding-left: 1cm;">
-<table border="0" cellspacing="0" cellpadding="4">
-
-<tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-win32-unicode-2.8.12.1-py26.exe">
-wxPython2.8-win32-unicode-py26</a>
-<br><a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-win32-ansi-2.8.12.1-py26.exe">
-wxPython2.8-win32-ansi-py26</a>
-</font></td>
-<td><font size="-1">32-bit Python 2.6</td>
-</tr>
-
-<tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-win64-unicode-2.8.12.1-py26.exe">
-wxPython2.8-win64-unicode-py26</a>
-</font></td>
-<td><font size="-1">64-bit Python 2.6</td>
-</tr>
-
-<tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-win32-unicode-2.8.12.1-py27.exe">
-wxPython2.8-win32-unicode-py27</a>
-<br><a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-win32-ansi-2.8.12.1-py27.exe">
-wxPython2.8-win32-ansi-py27</a>
-</font></td>
-<td><font size="-1">32-bit Python 2.7</td>
-</tr>
-
-<tr>
-<td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-win64-unicode-2.8.12.1-py27.exe">
-wxPython2.8-win64-unicode-py27</a>
-</font></td>
-<td><font size="-1">64-bit Python 2.7</td>
-</tr>
-</table></p>
-</div>
-
-
-<p>
-<font face="sans-serif" size="+1"><b>wxPython Demo for Windows</b></font><br>
-
-This installer contains the infamous wxPython demo, other samples, and wxWidgets documentation.
-
-<p><div style="padding-left: 1cm;">
-<table border="0" cellspacing="0" cellpadding="4">
-<tr>
-<td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-win32-docs-demos-2.8.12.1.exe">
-wxPython2.8-win32-docs-demos</a>
-</font></td>
-</tr></table>
-</div></p>
-
-
-
-<p>
-<font face="sans-serif" size="+1"><b>Windows Development Files</b></font><br>
-
-These archives contain the wxWidgets headers, libraries and other files that might be needed if you want to build your own wx extension modules that link with the same version of the wxWidgets DLLs distributed with wxPython.
-
-<p><div style="padding-left: 1cm;">
-<table border="0" cellspacing="0" cellpadding="4">
-<tr>
-<td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-win32-devel-2.8.12.1-msvc9.tar.bz2">
-wxPython2.8-win32-devel for MSVC 9</a><br>
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-win64-devel-2.8.12.1-msvc9x64.tar.bz2">
-wxPython2.8-win64-devel for MSVC 9</a>
-</font></td>
-</tr>
-</table>
-</div></p>
-
-
-
-
-<p>
-<font face="sans-serif" size="+1"><b>Mac OSX Binaries</b></font><br>
-The wxPython binaries for OSX are mountable disk images.  Simply double click to mount the image and then run the installer application in the image.  Download the image that matches the version of Python that you want to use it with, and unless you know for sure that you need the ansi build please get the <a href="http://wiki.wxpython.org/UnicodeBuild">Unicode</a> build.  These binaries should work on all versions of OSX from 10.3.9 onwards on either PPC or i386 architectures.  Since they use the Carbon API they are limited to running in 32-bit mode.
-
-
-<p><div style="padding-left: 1cm;">
-<table border="0" cellspacing="0" cellpadding="4">
-<tr>
-<tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-osx-unicode-2.8.12.1-universal-py2.6.dmg">
-wxPython2.8-osx-unicode-py2.6</a><br>
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-osx-ansi-2.8.12.1-universal-py2.6.dmg">
-wxPython2.8-osx-ansi-py2.6</a>
-</font></td>
-<td><font size="-1"></td>
-</tr>
-
-<tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-osx-unicode-2.8.12.1-universal-py2.7.dmg">
-wxPython2.8-osx-unicode-py2.7</a><br>
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-osx-ansi-2.8.12.1-universal-py2.7.dmg">
-wxPython2.8-osx-ansi-py2.7</a>
-</font></td>
-<td><font size="-1"></td>
-</tr>
-
-</td>
-</tr>
-</table>
-</div></p>
-
-
-<p>
-<font face="sans-serif" size="+1"><b>wxPython demo for Mac OSX</b></font><br>
-These disk images contain the wxPython demo, the documentation, some other samples and some tools.
-
-<p><div style="padding-left: 1cm;">
-<table border="0" cellspacing="0" cellpadding="4">
-
-<tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-osx-docs-demos-2.8.12.1-universal-py2.6.dmg">
-wxPython2.8-osx-docs-demos-py2.6</a>
-</font></td>
-<td></td>
-</tr>
-
-<tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.8-osx-docs-demos-2.8.12.1-universal-py2.7.dmg">
-wxPython2.8-osx-docs-demos-py2.7</a>
-</font></td>
-<td><font size="-1"></td>
-</tr>
-
-</td>
-</tr>
-</table>
-</div></p>
-
-
-
-<p>
-<font face="sans-serif" size="+1"><b>Linux Binaries</b></font><br>
-To get prebuilt binaries for Linux or other platforms, please search in your distro's package repository, or any 3rd party repositories that may be available to you.
-
-Ubuntu users can get information about the the wx APT repository <a href="http://wiki.wxpython.org/InstallingOnUbuntuOrDebian">here</a>.
-
-If all else fails you can build wxPython yourself from the source code, see the next section.
-
-
-<p>
-<font face="sans-serif" size="+1"><b>Source Code and Other Tarballs</b></font><br>
-If you need to build wxPython yourself then you can do so with the first file in this list and using these <a href="BUILD-2.8.html">build instructions</a>.
-<p>The other files here are useful for learning <a href="http://wiki.wxpython.org/How%20to%20Learn%20wxPython">how to use wxPython</a>.</p>
-
-<p><div style="padding-left: 1cm;">
-<table border="0" cellspacing="0" cellpadding="4">
-
-<tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython-src-2.8.12.1.tar.bz2">wxPython-src</a>
-</font></td>
-<td><font size="-1">All of the wxWidgets and wxPython source code.</td>
-</tr>
-
-<tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython-docs-2.8.12.1.tar.bz2">wxPython-docs</a>
-</font></td>
-<td><font size="-1">The plain HTML files for the wxWidgets documentation</td>
-</tr>
-
-<tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython-demo-2.8.12.1.tar.bz2">wxPython-demo</a>
-</font></td>
-<td><font size="-1">The source code for the wxPython demo and other sample applications.</td>
-</tr>
-
-</td>
-</tr>
-</table>
-</div></p>
-
-
-
-
-</div></p>
-</td></tr>
-</table>
-
-<img src="images/dot.jpg" width="37" height="36" alt="" border="0">
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 <a name="devel"></a>  
 <table width="100%" border="1" cellspacing="0" cellpadding="8" bgcolor="#BCB8E7">
 <tr><td>
-<font face="sans-serif" size="+1"><b>Development Release</b></font><br>
-These files are for the most recent release in the development <a href="http://wiki.wxpython.org/ReleaseSeries">release series</a> of wxPython, version <b>2.9.5.0</b>.  This being not a "stable" release does not mean that it is more buggy, only that the APIs are allowed to evolve in non-backwards compatible ways if needed, although that doesn't actually happen very often.</td></tr>
-<tr><td>
 
 
-<p>
+<p><a name="msw"></a>
 <font face="sans-serif" size="+1"><b>Windows Binaries</b></font><br>
 
 Choose an installer that matches the version of Python you will be using.  If you are using a 64-bit version of Python then make sure you also get a 64-bit wxPython, otherwise choose a 32-bit installer even if you are on a 64-bit version of Windows.  There is no longer a separate ansi and Unicode build, it's all Unicode now (although string objects passed to wx APIs will still be converted automatically if possible.) 
@@ -253,30 +27,30 @@ Choose an installer that matches the version of Python you will be using.  If yo
 <table border="0" cellspacing="0" cellpadding="4">
 
 <tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.9-win32-2.9.5.0-py26.exe">
-wxPython2.9-win32-py26</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxPython3.0-win32-3.0.0.0-py26.exe">
+wxPython3.0-win32-py26</a>
 </font></td>
 <td><font size="-1">32-bit Python 2.6</td>
 </tr>
 
 <tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.9-win64-2.9.5.0-py26.exe">
-wxPython2.9-win64-py26</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxPython3.0-win64-3.0.0.0-py26.exe">
+wxPython3.0-win64-py26</a>
 </font></td>
 <td><font size="-1">64-bit Python 2.6</td>
 </tr>
 
 <tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.9-win32-2.9.5.0-py27.exe">
-wxPython2.9-win32-py27</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxPython3.0-win32-3.0.0.0-py27.exe">
+wxPython3.0-win32-py27</a>
 </font></td>
 <td><font size="-1">32-bit Python 2.7</td>
 </tr>
 
 <tr>
 <td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.9-win64-2.9.5.0-py27.exe">
-wxPython2.9-win64-py27</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxPython3.0-win64-3.0.0.0-py27.exe">
+wxPython3.0-win64-py27</a>
 </font></td>
 <td><font size="-1">64-bit Python 2.7</td>
 </tr>
@@ -293,8 +67,8 @@ This installer contains the infamous wxPython demo, other samples, and wxWidgets
 <table border="0" cellspacing="0" cellpadding="4">
 <tr>
 <td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.9-win32-docs-demos-2.9.5.0.exe">
-wxPython2.9-win32-docs-demos.exe</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxPython3.0-win32-docs-demos-3.0.0.0.exe">
+wxPython3.0-win32-docs-demos.exe</a>
 </font></td>
 </tr></table>
 </div></p>
@@ -310,8 +84,8 @@ These installers contain the wxWidgets headers, libraries and other files that m
 <table border="0" cellspacing="0" cellpadding="4">
 <tr>
 <td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxWidgets2.9-devel-win32-2.9.5.0.exe">32-bit binaries for MSVC 9</a><br>
-<a href="http://downloads.sourceforge.net/wxpython/wxWidgets2.9-devel-win64-2.9.5.0.exe">64-bit binaries for MSVC 9</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxWidgets3.0-devel-win32-3.0.0.0.exe">32-bit binaries for MSVC 9</a><br>
+<a href="http://downloads.sourceforge.net/wxpython/wxWidgets3.0-devel-win64-3.0.0.0.exe">64-bit binaries for MSVC 9</a>
 </font></td>
 </tr>
 </table>
@@ -320,7 +94,7 @@ These installers contain the wxWidgets headers, libraries and other files that m
 
 
 
-<p>
+<p><a name="osx"></a>
 <font face="sans-serif" size="+1"><b>Mac OSX Binaries</b></font><br>
 The wxPython binaries for OSX are mountable disk images.  Simply double click to mount the image and then run the installer application in the image.  Be sure to download the image that matches the version of Python that you want to use it with.  The files with "carbon" in the name use the Carbon API for implementing the GUI, are compatible with PPC and i386 machines are will work on OSX 10.4 and onwards.  The file with "cocoa" in the name use the Cocoa API for implementing the GUI, requires at least OSX 10.5, and supports either 32-bit or 64-bit architectures.  If you would like to have both the Carbon and Cocoa versions installed you will need to do something like install the Carbon build for Python 2.6 and install the Cocoa build for Python 2.7.
 
@@ -328,22 +102,22 @@ The wxPython binaries for OSX are mountable disk images.  Simply double click to
 <p><div style="padding-left: 1cm;">
 <table border="0" cellspacing="0" cellpadding="4">
 <tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.9-osx-2.9.5.0-carbon-py2.6.dmg">
-wxPython2.9-osx-carbon-py2.6</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxPython3.0-osx-3.0.0.0-carbon-py2.6.dmg">
+wxPython3.0-osx-carbon-py2.6</a>
 </font></td>
 <td></td>
 </tr>
 
 <tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.9-osx-2.9.5.0-carbon-py2.7.dmg">
-wxPython2.9-osx-carbon-py2.7</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxPython3.0-osx-3.0.0.0-carbon-py2.7.dmg">
+wxPython3.0-osx-carbon-py2.7</a>
 </font></td>
 <td></td>
 </tr>
 
 <tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.9-osx-2.9.5.0-cocoa-py2.7.dmg">
-wxPython2.9-osx-cocoa-py2.7</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxPython3.0-osx-3.0.0.0-cocoa-py2.7.dmg">
+wxPython3.0-osx-cocoa-py2.7</a>
 </font></td>
 <td></td>
 </tr>
@@ -362,22 +136,22 @@ These disk images contain the wxPython demo, the documentation, some other sampl
 <table border="0" cellspacing="0" cellpadding="4">
 
 <tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.9-osx-docs-demos-2.9.5.0-carbon-py2.6.dmg">
-wxPython2.9-osx-docs-demos-carbon-py2.6</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxPython3.0-osx-docs-demos-3.0.0.0-carbon-py2.6.dmg">
+wxPython3.0-osx-docs-demos-carbon-py2.6</a>
 </font></td>
 <td></td>
 </tr>
 
 <tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.9-osx-docs-demos-2.9.5.0-carbon-py2.7.dmg">
-wxPython2.9-osx-docs-demos-carbon-py2.7</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxPython3.0-osx-docs-demos-3.0.0.0-carbon-py2.7.dmg">
+wxPython3.0-osx-docs-demos-carbon-py2.7</a>
 </font></td>
 <td></td>
 </tr>
 
 <tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython2.9-osx-docs-demos-2.9.5.0-cocoa-py2.7.dmg">
-wxPython2.9-osx-docs-demos-cocoa-py2.7</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxPython3.0-osx-docs-demos-3.0.0.0-cocoa-py2.7.dmg">
+wxPython3.0-osx-docs-demos-cocoa-py2.7</a>
 </font></td>
 <td></td>
 </tr>
@@ -389,7 +163,7 @@ wxPython2.9-osx-docs-demos-cocoa-py2.7</a>
 
 
 
-<p>
+<p><a name="linux"></a>
 <font face="sans-serif" size="+1"><b>Linux Binaries</b></font><br>
 To get prebuilt binaries for Linux or other platforms, please search in your distro's package repository, or any 3rd party repositories that may be available to you.
 
@@ -398,7 +172,7 @@ Ubuntu users can get information about the the wx APT repository <a href="http:/
 If all else fails you can build wxPython yourself from the source code, see the next section.
 
 
-<p>
+<p><a name="source"></a>
 <font face="sans-serif" size="+1"><b>Source Code and Other Tarballs</b></font><br>
 If you need to build wxPython yourself then you can do so with the first file in this list and using these <a href="BUILD.html">build instructions</a>.
 <p>The other files here are useful for learning <a href="http://wiki.wxpython.org/How%20to%20Learn%20wxPython">how to use wxPython</a>.</p>
@@ -407,19 +181,19 @@ If you need to build wxPython yourself then you can do so with the first file in
 <table border="0" cellspacing="0" cellpadding="4">
 
 <tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython-src-2.9.5.0.tar.bz2">wxPython-src</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxPython-src-3.0.0.0.tar.bz2">wxPython-src</a>
 </font></td>
 <td><font size="-1">All of the wxWidgets and wxPython source code.</td>
 </tr>
 
 <tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython-docs-2.9.5.0.tar.bz2">wxPython-docs</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxPython-docs-3.0.0.0.tar.bz2">wxPython-docs</a>
 </font></td>
 <td><font size="-1">The plain HTML files for the wxWidgets documentation</td>
 </tr>
 
 <tr><td nowrap><font size="-1">
-<a href="http://downloads.sourceforge.net/wxpython/wxPython-demo-2.9.5.0.tar.bz2">wxPython-demo</a>
+<a href="http://downloads.sourceforge.net/wxpython/wxPython-demo-3.0.0.0.tar.bz2">wxPython-demo</a>
 </font></td>
 <td><font size="-1">The source code for the wxPython demo and other sample applications.</td>
 </tr>

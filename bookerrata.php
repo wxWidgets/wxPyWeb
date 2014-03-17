0a10f219e0f299f0c5100f972a2da80b594112be
Robin Dunn <robin@alldunn.com>
2014-03-17 15:48:13 +0000

    Fix published errata link
    git-svn-id: https://svn.wxwidgets.org/svn/wxPyWeb/trunk@85 dae2d619-a531-0410-97b3-f94ca8335064

--- bookerrata.php
+++ bookerrata.php
@@ -3,6 +3,6 @@
 </HEAD>
 
 <?php require('_frame_before.in') ?>
-http://docs.google.com/Doc?id=ah2sk2p9kmhc_bd7prph4rzxj
+https://docs.google.com/document/d/1WRKS12S3IjUp9YY-D8SAzbbzy-q9VnutZK71hyGRfWc/pub
 <?php require('_frame_after.in') ?>
 

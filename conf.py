29c31512c32aa8cdef68f98f0127b39076b0d2fc
Robin Dunn <robin@alldunn.com>
2017-07-18 18:39:59 -0700

    Tweak the footer

--- conf.py
+++ conf.py
@@ -1019,7 +1019,10 @@ LICENSE = ""
 
 # A small copyright notice for the page footer (in HTML).
 # (translatable)
-CONTENT_FOOTER = 'Contents &copy; {date}         <a href="mailto:{email}">{author}</a> - Powered by         <a href="https://getnikola.com" rel="nofollow">Nikola</a>         {license}'
+CONTENT_FOOTER = 'Contents &copy; {date}  {author} - ' \
+                 'Powered by <a href="https://getnikola.com" rel="nofollow">Nikola</a>, '\
+                 '<a href="https://python.org" rel="nofollow">Python</a> and Magic. '\
+                 '{license}'
 
 # Things that will be passed to CONTENT_FOOTER.format().  This is done
 # for translatability, as dicts are not formattable.  Nikola will
